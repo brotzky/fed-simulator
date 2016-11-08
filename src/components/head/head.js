b755881f2c73217d1a0c9eddf49df238b1bf35d4
Aaron Lote (BTLAPTOP) <aaron.lote@bt.com>
2016-11-08 17:14:54 +0000

    Set Baseurl

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,11 +1,17 @@
 import React from "react"
 import Helmet from "react-helmet"
+import constants from "../../constants"
 
 const Head = () => {
   return (
     <Helmet
       titleTemplate="%s - Welcome"
       defaultTitle="Welcome"
+      base={
+        {
+          "href": constants.baseUrl,
+        }
+      }
     />
   )
 }
