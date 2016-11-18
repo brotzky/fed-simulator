74336454b9f5727cf3e8836c02f2b834585bb8b8
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-11-18 07:49:10 +0000

    Match working

--- match.config.js
+++ match.config.js
@@ -13,37 +13,38 @@ const randomEvents = [
   },
 ]
 
-const commonMoves = [
-  {
-    name: "punch",
-    damage: 3,
-    stamina: 0.1,
-  },
-  {
-    name: "kick",
-    damage: 3,
-    stamina: 0.1,
-  },
-  {
-    name: "finisher",
-    damage: 8,
-    stamina: 0.5,
-  },
+
+const moves = [
+  {name: "Punch", weight: 0.5, damage: 1},
+  {"name": "Kick", weight: 0.5, damage: 2},
+  // "Suplex": {weight: 0.08, damage: 5},
+  // "Bodyslam": {weight: 0.02, damage: 5},
+  // "Powerbomb": {weight: 0.03, damage: 5},
+  // "Arm bar": {weight: 0.05, damage: 5},
+  // "Big back body drop": {weight: 0.08, damage: 5},
+  // "Clothesline": {weight: 0.05, damage: 5},
+  // "Eyepoke": {weight: 0.08, damage: 5},
+  // "Locked in a submission": {weight: 0.1, damage: 7},
+  // "Superkick": {weight: 0.05, damage: 9},
+  // "Back suplex": {weight: 0.05, damage: 2},
+  // "Back rake": {weight: 0.05, damage: 2},
+  // "Came off the top rope": {weight: 0.05, damage: 10},
+  // "Signature move": {weight: 0.01, damage: 12},
+  // "Finishing move": {weight: 0.09, damage: 29},
+  // "PILEDRIVER!": {weight: 0.02, damage: 15},
 ]
 
 const wrestlers = [
   {
     name: "Jericho",
-    stamina: 0.9,
     damage: 89,
-    moves: commonMoves,
+    moves,
   },
   {
     name: "HHH",
-    stamina: 1,
     damage: 91,
-    moves: commonMoves,
+    moves,
   },
 ]
 
-export { wrestlers, randomEvents, maxMoves }
+export { moves, wrestlers, randomEvents, maxMoves }
--- match.js
+++ match.js
@@ -1,46 +1,67 @@
-import { wrestlers, randomEvents, maxMoves } from "./match.config"
+import { moves, wrestlers, randomEvents, maxMoves } from "./match.config"
 import chalk from "chalk"
+import util from "util"
 import "./match.prototype"
+import weighted from "weighted"
 
 let
   log = console.log,
+  standout = chalk.cyan,
   good = chalk.green,
   bad = chalk.red,
   neutral = chalk.yellow
 
+log(neutral(
+  "Common Moves Count: ",
+
+))
+
 class Match {
-
-  constructor(wrestlers) {
+  constructor(wrestlers, moves) {
+    wrestlers.forEach((wrestler, wrestlerKey) => {
+      wrestlers[wrestlerKey] = wrestler
+    })
     this.wrestlers = wrestlers
+    this.moves = moves
+    this.movesWeights = moves.reduce((a, b) => a.concat(b.weight), [])
     this.maxMoves = maxMoves
-    log(neutral(wrestlers.map((wrestler) => wrestler.name)))
+    log(neutral(
+      "Common Moves Count: ", this.countTotalMoveDamage(moves)
+    ))
+    log(standout(wrestlers.map((wrestler) => wrestler.name)))
   }
 
-  hitMove = () => {
-    let
-      wrestler = this.wrestlers.random(),
-      move = wrestler.moves.random()
-    log(`${wrestler.name} hit a ${move.name} for ${move.damage} damage`)
-    if (this.maxMoves-- === 0) {
-      clearInterval(this.currentInterval)
-      this.endMatch()
-    }
+  countTotalMoveDamage(moves) {
+    return Object.keys(moves).reduce((previous, key) => {
+      return previous + moves[key].weight
+    }, 0)
   }
-
-  endMatch() {
+  endMatch = () => {
     log(good("Match ended with a time out"))
+  }
+
+  hitMove = (attacker, defender, move) => {
+    this.wrestlers.forEach((wrestler, key) => {
+      if (defender.name === wrestler.name) {
+        this.wrestlers[key].damage = wrestler.damage - move.damage
+        log(neutral(`
+          ${attacker.name}(${attacker.damage}) used ${move.name} on ${defender.name}(${defender.damage}) for ${move.damage} damage
+        `))
+      }
+    })
   }
 
   ringBell() {
     log(good("Bell Rings"))
-    this.currentInterval = setInterval(
-      this.hitMove
-      , 200
-    )
+    while(this.wrestlers[0].damage > 10 || this.wrestlers[1].damage > 10) {
+      let
+        attacker = weighted.select(this.wrestlers, [0.5, 0.5]),
+        defender = this.wrestlers.filter((wrestler) => wrestler.name !== attacker.name)[0],
+        move =  weighted.select(this.moves, this.movesWeights)
+      this.hitMove(attacker, defender, move)
+    }
   }
-
 }
 
-
-let currentMatch = new Match(wrestlers)
+let currentMatch = new Match(wrestlers, moves)
 currentMatch.ringBell()
