15526322d8c0350f1d4748bc88cbf992e970736e
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-11-22 17:26:31 +0000

    Wrap story in a col so it has auto padding

--- src/components/match/story.js
+++ src/components/match/story.js
@@ -9,30 +9,32 @@ export default class Story extends React.Component {
 
   render() {
     return (
-      <ul className="story">
-        <If condition={this.props.collection.length > 0}>
-          {this.props.collection.map((action, key) => {
-            return (
-              <li
-                className={`story__action story--${action.action}`}
-                key={key}>
-                <Choose>
-                  <When condition={action.action === "move"}>
-                    <span className={`story__attacker actor--${toSlug(action.details.attacker.bucket)}`}>{action.details.attacker.name}</span> hit&nbsp;
-                    <span className={`story__defender actor--${toSlug(action.details.defender.bucket)}`}>{action.details.defender.name}</span> with&nbsp;
-                    <span className="story__move">{action.details.move.name}</span> for	&nbsp;
-                    <span className="story__damage">{action.details.move.damage}</span> damage
-                  </When>
-                  <When condition={action.action === "winner"}>
-                    <span className={`story__winner actor--${toSlug(action.details.winner.bucket)}`}>{action.details.winner.name}</span> pins&nbsp;
-                    <span className={`story__loser actor--${toSlug(action.details.loser.bucket)}`}>{action.details.loser.name}</span> for the win!
-                  </When>
-                </Choose>
-              </li>
-            )
-          })}
-        </If>
-      </ul>
+      <div className="col-xs-12">
+        <ul className="story">
+          <If condition={this.props.collection.length > 0}>
+            {this.props.collection.map((action, key) => {
+              return (
+                <li
+                  className={`story__action story--${action.action}`}
+                  key={key}>
+                  <Choose>
+                    <When condition={action.action === "move"}>
+                      <span className={`story__attacker actor--${toSlug(action.details.attacker.brand)}`}>{action.details.attacker.name}</span> hit&nbsp;
+                      <span className={`story__defender actor--${toSlug(action.details.defender.brand)}`}>{action.details.defender.name}</span> with&nbsp;
+                      <span className="story__move">{action.details.move.name}</span> for	&nbsp;
+                      <span className="story__damage">{action.details.move.damage}</span> damage
+                    </When>
+                    <When condition={action.action === "winner"}>
+                      <span className={`story__winner actor--${toSlug(action.details.winner.brand)}`}>{action.details.winner.name}</span> pins&nbsp;
+                      <span className={`story__loser actor--${toSlug(action.details.loser.brand)}`}>{action.details.loser.name}</span> for the win!
+                    </When>
+                  </Choose>
+                </li>
+              )
+            })}
+          </If>
+        </ul>
+      </div>
     )
   }
 }
