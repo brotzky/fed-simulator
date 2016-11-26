2c58a9c9e161652b14eaf4681a0362cec46f8ca3
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-11-26 16:11:59 +0000

    New source path for sprite generation

--- config/webpack.config.sprite.js
+++ config/webpack.config.sprite.js
@@ -3,12 +3,12 @@ const sprite = require("sprite-webpack-plugin")
 module.exports = {
   plugins: [
     new sprite({
-      source : "src/imgs/wrestlers/",
+      source : "scripts/generate/import/",
       imgPath: "src/imgs/",
       cssPath: "src/stylesheets/",
       processor: "scss",
       spriteName: "wrestlers",
       orientation: "horizontal",
-    })
-  ]
+    }),
+  ],
 }
