5d2f4860679f2c75201f7a83280205e5355fad23
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-02 14:39:24 +0000

    Calculate widths of moves based on damage

--- src/components/story/story.js
+++ src/components/story/story.js
@@ -9,15 +9,25 @@ export default class Story extends React.Component {
   }
 
   render() {
+    let totalWidthPoints = this.props.collection
+      .filter((action) => action.action === "move")
+      .reduce((sum, action) => {
+        return sum + action.details.move.damage
+      }, 0)
+    console.log(totalWidthPoints)
     return (
       <ul className="col-xs-12 story">
         <If condition={this.props.collection.length > 0}>
           {this.props.collection.map((action, key) => {
-            let brand = action.details.attacker
-              ? action.details.attacker.brand
-              : action.details.winner.brand
+            let
+              brand = action.details.attacker
+                ? action.details.attacker.brand
+                : action.details.winner.brand,
+              width = (action.action === "move")
+                ? 100 * action.details.move.damage / totalWidthPoints
+                : 100
             return (
-              <li
+              <li style={ {width: `${width}%`} }
                 className={`story__action story--${action.action} brand--${toSlug(brand)}`}
                 key={key}>
                 <Choose>
