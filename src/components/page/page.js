dcc8cccebfcad077111408630544163c46fec266
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-06 23:31:29 +0000

    constant for url generation in page level navigation

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -4,8 +4,31 @@ import Head from "../head/head"
 import "../../stylesheets/base"
 import logo from "./logo.png"
 import "./stylesheets/main"
+const navigationItems = [
+  {
+    url: "draft",
+    title: "Draft",
+  },
+  {
+    url: "champions",
+    title: "Champions",
+  },
+  {
+    url: "ppvs",
+    title: "PPVs",
+  },
+  {
+    url: "ranking",
+    title: "Ranking",
+  },
+  {
+    url: "show",
+    title: "Create a show",
+  },
+]
 
 export default class Page extends React.Component {
+
   render() {
     return (
       <div>
@@ -18,36 +41,16 @@ export default class Page extends React.Component {
                 }} /> Universe
               </a>
             </div>
-            <div className="navigation__item">
-              <Link
-                to="draft">
-                Draft
-              </Link>
-            </div>
-            <div className="navigation__item">
-              <Link
-                to="champions">
-                Champions
-              </Link>
-            </div>
-            <div className="navigation__item">
-              <Link
-                to="ranking">
-                Ranking
-              </Link>
-            </div>
-            <div className="navigation__item">
-              <Link
-                to="show">
-                Show
-              </Link>
-            </div>
-            <div className="navigation__item">
-              <Link
-                to="ppvs">
-                PPVs
-              </Link>
-            </div>
+            {navigationItems.map((navigationItem, key) => {
+              return (
+                <div key={key} className="navigation__item">
+                  <Link
+                    to={navigationItem.url}>
+                    {navigationItem.title}
+                  </Link>
+                </div>
+              )
+            })}
           </div>
         </header>
         <section className="container-fluid">
