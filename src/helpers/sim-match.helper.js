6539d9f280c745746b9d65dbf227dfcfcc4d095f
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-09 01:39:51 +0000

    The highest rated wrestler cannibalises 50% of the opponents change of hitting a move, this leads to a much better win / loss ration

--- src/helpers/sim-match.helper.js
+++ src/helpers/sim-match.helper.js
@@ -1,6 +1,7 @@
 import weighted from "weighted"
 import _maxBy from "lodash.maxby"
 import _minBy from "lodash.minby"
+const getWrestlersWeights = (arrayLength) => new Array(arrayLength).fill((1 / arrayLength))
 
 export class SimMatch {
 
@@ -26,10 +27,22 @@ export class SimMatch {
     // while the minimum damage done is stil above zero
     while(_minBy(this.wrestlers, "damage").damage > 0) {
       let
-        getWrestlersWeights = (wrestlers) => new Array(wrestlers.length).fill((1 / wrestlers.length)),
-        attacker = weighted.select(this.wrestlers, getWrestlersWeights(this.wrestlers)),
-        defenders = this.wrestlers.filter((wrestler) => wrestler.name !== attacker.name),
-        defender = weighted.select(defenders, getWrestlersWeights(defenders)),
+        lowestId = this.wrestlers.sort((a, b) => a.damage > b.damage)[0].id,
+        highestId = this.wrestlers.sort((a, b) => a.damage < b.damage)[0].id,
+        lowestIndex = this.wrestlers.findIndex((wrestler, index) => wrestler.id === lowestId),
+        highestIndex = this.wrestlers.findIndex((wrestler, index) => wrestler.id === highestId),
+        attackersWeights = getWrestlersWeights(this.wrestlers.length)
+
+        attackersWeights[lowestIndex] = attackersWeights[lowestIndex] - (attackersWeights[lowestIndex] * 0.5)
+        attackersWeights[highestIndex] = attackersWeights[highestIndex] + attackersWeights[lowestIndex]
+        console.log("attackersWeightTotal", attackersWeights.reduce((sum, weight) => sum + weight), attackersWeights)
+
+        let attacker = weighted.select(this.wrestlers, attackersWeights)
+        let defenderWeights = Object.assign({}, attackersWeights)
+        delete defenderWeights[highestIndex]
+
+        let defenders = this.wrestlers.filter((wrestler) => wrestler.name !== attacker.name),
+        defender = weighted.select(defenders, getWrestlersWeights(defenders.length)),
         move =  weighted.select(this.moves, this.movesWeights)
       this.hitMove(attacker, defender, move)
     }
