efc4b391d92977469c71d6f4d122a8d107bb4f4b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-09 02:51:02 +0000

    Work on getting a better match balance by choosing a random wrestler first and then basing the match off a range on their damage

--- src/helpers/match.js
+++ src/helpers/match.js
@@ -3,13 +3,9 @@ import { SimMatch } from "./sim-match.helper"
 import { awardMatchPoints } from "../actions/wrestlers"
 import { shouldTheChampionshipMove } from "../actions/championship"
 
-const getWeightedArrayOfLength = (length) => {
-  return new Array(length).fill((1 / length))
-}
-
-const getWrestler = (wrestlers) => {
-  return weighted.select(wrestlers, getWeightedArrayOfLength(wrestlers.length))
-}
+const between = (number, lower, upper) => number > lower && number < upper
+const getWeightedArrayOfLength = (length) => new Array(length).fill((1 / length))
+const getWrestler = (wrestlers) => weighted.select(wrestlers, getWeightedArrayOfLength(wrestlers.length))
 
 const defaultSettings = {
   male: {
@@ -19,6 +15,10 @@ const defaultSettings = {
   amount: {
     options: [2, 3, 4, 5, 6],
     weights: [0.5, 0.2, 0.2, 0.05, 0.05]
+  },
+  range: {
+    min: 10,
+    max: 10,
   }
 }
 
@@ -31,8 +31,21 @@ export function randomiseWrestlers(
     amountOfWrestlers = weighted.select(settings.amount.options, settings.amount.weights),
     filteredWrestlers = wrestlers.filter(wrestler => wrestler.male === randomBool)
 
-  while (amountOfWrestlers > 0) {
+    // pick our first wrestler, we do this so we can get a fair match
+    let firstWrestler = getWrestler(filteredWrestlers),
+      firstWrestlerMin = firstWrestler.rating - settings.range.min,
+      firstWrestlerMax = firstWrestler.rating + settings.range.max
+      // push first wrestler to match
+      matchWrestlers.push(
+        firstWrestler
+      )
+    // filter wrestlers out not in the first wrestlers range
+    filteredWrestlers = filteredWrestlers.filter(wrestler => between(wrestler.rating, firstWrestlerMin, firstWrestlerMax))
+
+  // while amount to create is above one
+  while (amountOfWrestlers > 1) {
     let chosenWrestler = getWrestler(filteredWrestlers)
+    // we don't want the same wrestler in the match, so drop them out
     filteredWrestlers = filteredWrestlers.filter((wrestler) => wrestler.id !== chosenWrestler.id)
     matchWrestlers.push(
       chosenWrestler
