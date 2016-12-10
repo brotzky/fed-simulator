344cd670fe8af1a61fd3f223432185d7ae21d715
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-10 02:18:34 +0000

    Allow championships to move brands and record the number of title changes

--- src/reducers/championships.js
+++ src/reducers/championships.js
@@ -1,30 +1,34 @@
 import defaultState from "./championships.default"
 
 export default (state = defaultState, action) => {
-  let newState = JSON.parse(JSON.stringify(state))
+  let newState = JSON.parse(JSON.stringify(state)),
+    key = 0
 
   switch (action.type) {
     case "MOVE_CHAMPIONSHIP":
-      let key = newState.findIndex(championship => championship.id === action.championship.id)
+      key = newState.findIndex(championship => championship.id === action.championship.id)
 
       newState[key].wrestlers.push(action.wrestler)
+      newState[key].brand = action.wrestler.brand
+
       let newLength = newState[key].wrestlers.length
 
-      if ( (newState[key].tag && newLength === 3) || (!newState[key].tag && newLength === 2)) {
+      if ((newState[key].tag && newLength === 3) || (!newState[key].tag && newLength === 2)) {
         newState[key].wrestlers.shift()
       }
-
       break
     case "SHOULD_MOVE_CHAMPIONSHIP":
-      let movedOneChampionship = 0
-
       newState.forEach((championship, key) => {
-        if (championship.wrestlers[0] && championship.wrestlers[0].id && championship.wrestlers[0].id === action.loser.id && action.winner.male === championship.male && movedOneChampionship === 0) {
-          newState[key].wrestlers = [
-            {...action.winner}
-          ]
-          movedOneChampionship++
-        }
+        championship.wrestlers.forEach((wrestler, wrestlerKey) => {
+          if (wrestler.id === action.loser.id) {
+            if (championship.canMoveBrands) {
+              newState[key].brand = action.winner.brand
+            }
+            newState[key].changes += 1
+            newState[key].wrestlers = newState[key].wrestlers.filter(wrestler => wrestler.id !== action.loser.id)
+            newState[key].wrestlers.push(action.winner)
+          }
+        })
       })
       break
     case "CLEAR_CHAMPIONS":
@@ -34,6 +38,8 @@ export default (state = defaultState, action) => {
       break
     case "RESET_CHAMPIONS":
       newState = defaultState
+      break
+    default:
       break
   }
   return newState
