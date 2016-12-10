d11557235227ed72600747e24899edae656eb78b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-10 20:01:31 +0000

    React sticky nav

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -2,11 +2,11 @@ import React from "react"
 import { Link } from "react-router"
 import Head from "../head/head"
 import { connect } from "react-redux"
+import { StickyContainer } from "react-sticky"
 import "drag-drop-polyfill";
 import "drag-drop-polyfill/release/drag-drop-polyfill"
 import * as versionActions from "../../actions/version"
 import navigation from "./navigation"
-
 import logo from "./logo.png"
 import "../../stylesheets/base"
 
@@ -54,19 +54,21 @@ class Page extends React.Component {
             </ul>
           </div>
         </header>
-        <section className="container-fluid">
-          {this.props.children}
-          <div className="row legal">
-            <div className="col-xs-12">
-              <p>
-                All WWE imagery is taken from <a href="http://www.wwe.com/main-help/generalfaq/copyright" target="_blank">WWE.com</a> and owned by <a href="https://wwe.com" target="_blank">World Wrestling Entertainment, Inc.</a>
-              </p>
-              <p>
-                Please <a href="mailto:aaron.lote@gmail.com">email me</a> directly with any queries or find me on <a target="_blank" href="https://twitter.com/azz0r">twitter @azz0r</a>
-              </p>
+        <StickyContainer>
+          <section className="container-fluid">
+            {this.props.children}
+            <div className="row legal">
+              <div className="col-xs-12">
+                <p>
+                  All WWE imagery is taken from <a href="http://www.wwe.com/main-help/generalfaq/copyright" target="_blank">WWE.com</a> and owned by <a href="https://wwe.com" target="_blank">World Wrestling Entertainment, Inc.</a>
+                </p>
+                <p>
+                  Please <a href="mailto:aaron.lote@gmail.com">email me</a> directly with any queries or find me on <a target="_blank" href="https://twitter.com/azz0r">twitter @azz0r</a>
+                </p>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </StickyContainer>
       </div>
     )
   }
--- src/components/page/secondary.js
+++ src/components/page/secondary.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Icon from "../icon/icon"
 import * as wrestlersActions from "../../actions/wrestlers"
+import { Sticky } from "react-sticky"
 import { connect } from "react-redux"
 import { randomiseWrestlers, simulateMatch, logMatch } from "../../helpers/match"
 const amountOfSims = [
@@ -48,36 +49,38 @@ class PageSecondary extends React.Component {
 
   render() {
     return (
-      <div className="navigation navigation--secondary">
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <a
-              onKeyPress={this.onReset}
-              onClick={this.onReset}>
-              Clear wins & losses
-            </a>
-          </li>
-          {this.props.brands.map((brand, key) => {
-            return (
-              <li key={key}
-                className="navigation__item">
-                <Icon name={brand.name} /> &nbsp;
-                {amountOfSims.map((amount, key) => {
-                  return (
-                    <span key={key}>
-                      <a
-                        onKeyPress={this.onSimulateBrandMatches.bind(this, { amount, brand })}
-                        onClick={this.onSimulateBrandMatches.bind(this, { amount, brand })}>
-                        {amount}
-                      </a>,	&nbsp;
-                    </span>
-                  )
-                })}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <Sticky>
+        <div className="navigation navigation--secondary">
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a
+                onKeyPress={this.onReset}
+                onClick={this.onReset}>
+                Clear wins & losses
+              </a>
+            </li>
+            {this.props.brands.map((brand, key) => {
+              return (
+                <li key={key}
+                  className="navigation__item">
+                  <Icon name={brand.name} /> &nbsp;
+                  {amountOfSims.map((amount, key) => {
+                    return (
+                      <span key={key}>
+                        <a
+                          onKeyPress={this.onSimulateBrandMatches.bind(this, { amount, brand })}
+                          onClick={this.onSimulateBrandMatches.bind(this, { amount, brand })}>
+                          {amount}
+                        </a>,	&nbsp;
+                      </span>
+                    )
+                  })}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </Sticky>
     )
   }
 }
