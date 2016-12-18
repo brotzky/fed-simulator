acb250201c1cf7ea350fa8a7c1eab539953c089e
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-18 12:19:29 +0000

    Added getRandomInt helper

--- src/helpers/math.js
+++ src/helpers/math.js
@@ -1,3 +1,8 @@
 export function getPercentageAmount(total, percentage) {
   return total * (percentage / 100)
 }
+
+// http://stackoverflow.com/a/24152886/275218
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
