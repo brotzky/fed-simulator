8fea54217202aa238de7d04ee6362088ec6392b6
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-18 12:33:57 +0000

    Tweak match randomiser so the higher rated wrestler gets a 20% of their chance of hitting a move

--- src/helpers/sim-match.helper.js
+++ src/helpers/sim-match.helper.js
@@ -1,6 +1,7 @@
 import weighted from "weighted"
 import _maxBy from "lodash.maxby"
 import _minBy from "lodash.minby"
+import { getPercentageAmount } from "./math"
 const getWrestlersWeights = (arrayLength) => new Array(arrayLength).fill((1 / arrayLength))
 
 export class SimMatch {
@@ -24,15 +25,20 @@ export class SimMatch {
   }
 
   ringBell() {
-    let
-      lowestId = this.wrestlers.sort((a, b) => a.damage > b.damage)[0].id,
-      highestId = this.wrestlers.sort((a, b) => a.damage < b.damage)[0].id,
-      lowestIndex = this.wrestlers.findIndex((wrestler, index) => wrestler.id === lowestId),
-      highestIndex = this.wrestlers.findIndex((wrestler, index) => wrestler.id === highestId),
-      attackersWeights = getWrestlersWeights(this.wrestlers.length)
+    let lowest = this.wrestlers.sort((a, b) => a.damage > b.damage)[0],
+      highest = this.wrestlers.sort((a, b) => a.damage < b.damage)[0],
+      lowestId = lowest.id,
+      highestId = highest.id,
+      lowestIndex = this.wrestlers.findIndex(wrestler => wrestler.id === lowestId),
+      highestIndex = this.wrestlers.findIndex(wrestler => wrestler.id === highestId),
+      attackersWeights = getWrestlersWeights(this.wrestlers.length),
+      highestAttackersPercentageGain =  getPercentageAmount(attackersWeights[lowestIndex], 20)
 
-      attackersWeights[lowestIndex] = attackersWeights[lowestIndex] - (attackersWeights[lowestIndex] * 0.5)
-      attackersWeights[highestIndex] = attackersWeights[highestIndex] + attackersWeights[lowestIndex]
+      if (lowest.damage !== highest.damage) {
+        attackersWeights[lowestIndex] = attackersWeights[lowestIndex] - highestAttackersPercentageGain
+        attackersWeights[highestIndex] = attackersWeights[highestIndex] + highestAttackersPercentageGain
+      }
+      // console.log(highestAttackersPercentageGain, attackersWeights[lowestIndex], attackersWeights[highestIndex])
       // console.log("attackersWeightTotal", attackersWeights.reduce((sum, weight) => sum + weight), attackersWeights)
 
     // while the minimum damage done is stil above zero
