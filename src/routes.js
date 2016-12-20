4c1851079c694a2099635426e059e3fc52537979
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-12-20 13:52:31 +0000

    Indentation

--- src/routes.js
+++ src/routes.js
@@ -4,69 +4,69 @@ import { IndexRoute, Route } from "react-router"
 
 export default () => {
   return (
-      <Route
-        path="/"
-        component={Page}>
+    <Route
+      path="/"
+      component={Page}>
+      <IndexRoute
+        getComponent={(nextState, callback) => {
+          require.ensure([], (require) => {
+            callback(null, require("./pages/landing/landing").default)
+          })
+        }}
+       />
+       <Route path="show">
+         <IndexRoute
+           getComponent={(nextState, callback) => {
+             require.ensure([], (require) => {
+               callback(null, require("./pages/show/show").default)
+             })
+           }}
+         />
+       </Route>
+       <Route path="champions">
+         <IndexRoute
+           getComponent={(nextState, callback) => {
+             require.ensure([], (require) => {
+               callback(null, require("./pages/champions/champions").default)
+             })
+           }}
+         />
+       </Route>
+       <Route path="ppvs">
+         <IndexRoute
+           getComponent={(nextState, callback) => {
+             require.ensure([], (require) => {
+               callback(null, require("./pages/ppvs/ppvs").default)
+             })
+           }}
+         />
+       </Route>
+       <Route path="ranking">
+         <IndexRoute
+           getComponent={(nextState, callback) => {
+             require.ensure([], (require) => {
+               callback(null, require("./pages/ranking/ranking").default)
+             })
+           }}
+         />
+      </Route>
+      <Route path="draft">
         <IndexRoute
           getComponent={(nextState, callback) => {
             require.ensure([], (require) => {
-              callback(null, require("./pages/landing/landing").default)
+              callback(null, require("./pages/draft/draft").default)
             })
           }}
-         />
-         <Route path="show">
-           <IndexRoute
-             getComponent={(nextState, callback) => {
-               require.ensure([], (require) => {
-                 callback(null, require("./pages/show/show").default)
-               })
-             }}
-           />
-         </Route>
-         <Route path="champions">
-           <IndexRoute
-             getComponent={(nextState, callback) => {
-               require.ensure([], (require) => {
-                 callback(null, require("./pages/champions/champions").default)
-               })
-             }}
-           />
-         </Route>
-         <Route path="ppvs">
-           <IndexRoute
-             getComponent={(nextState, callback) => {
-               require.ensure([], (require) => {
-                 callback(null, require("./pages/ppvs/ppvs").default)
-               })
-             }}
-           />
-         </Route>
-         <Route path="ranking">
-           <IndexRoute
-             getComponent={(nextState, callback) => {
-               require.ensure([], (require) => {
-                 callback(null, require("./pages/ranking/ranking").default)
-               })
-             }}
-           />
-        </Route>
-        <Route path="draft">
-          <IndexRoute
-            getComponent={(nextState, callback) => {
-              require.ensure([], (require) => {
-                callback(null, require("./pages/draft/draft").default)
-              })
-            }}
-          />
-        </Route>
-        <Route
-          path="*"
-          getComponent={(nextState, callback) => {
-            require.ensure([], (require) => {
-              callback(null, require("./pages/landing/landing").default)
-            })
-          }}
-         />
+        />
+      </Route>
+      <Route
+        path="*"
+        getComponent={(nextState, callback) => {
+          require.ensure([], (require) => {
+            callback(null, require("./pages/landing/landing").default)
+          })
+        }}
+      />
     </Route>
   )
 }
