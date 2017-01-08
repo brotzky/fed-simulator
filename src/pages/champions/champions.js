a83622cf476356f679d6f827c8be98c9ca79cfb7
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-08 22:57:36 +0000

    Strip championship page

--- src/pages/champions/champions.js
+++ src/pages/champions/champions.js
@@ -26,11 +26,9 @@ class ChampionsPage extends React.Component {
   }
 
   render() {
-    const awardedChampionships = this.props.championships.filter(championship => championship.wrestlerId !== "" || championship.wrestlerIds.length > 0).length
     return (
       <main className="page champions">
         <Helmet title="Championships" />
-        <Secondary />
         <div className="navigation navigation--secondary">
           <ul className="navigation__list">
             <li className="navigation__item">
@@ -43,21 +41,15 @@ class ChampionsPage extends React.Component {
           </ul>
         </div>
         <div className="inpage-content">
-          <If condition={awardedChampionships < 3}>
-            <div className="alert alert-info" role="alert">
-              Drag and drop wrestlers onto a championship to award it to them!
-            </div>
-          </If>
           <div className="row">
-            {this.props.brands.filter(brand => brand.default === false).map((brand, key) => {
+            {this.props.brands.filter(brand => brand.default === false).map((brand) => {
               let wrestlers = this.props.wrestlers
                 .filter(wrestler => wrestler.brand === brand.name),
                 championships = this.props.championships
                   .filter(championship => championship.brand === brand.name)
                   .sort((a, b) => a.rating > b.rating ? 1 : -1)
               return (
-                <div
-                  key={brand.id}
+                <div key={brand.id}
                   className="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                     <div className="row clearfix">
                       <div className="col-xs-12">
