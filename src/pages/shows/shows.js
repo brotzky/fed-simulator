0f3571d6aacde98dae599e1c6f9f4a8d6bb6a0b8
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-16 17:13:58 +0000

    Improved defaults width

--- src/pages/shows/shows.js
+++ src/pages/shows/shows.js
@@ -77,7 +77,7 @@ class ShowsPage extends React.Component {
                           {show.PPV.name}
                         </Link>
                       </td>
-                      <td><Icon name={show.brand.name} /></td>
+                      <td>{show.brand.name}</td>
                       <td>{show.date}</td>
                       <td>{show.matches.filter(match => match.wrestlers && match.wrestlers.length > 0).length}</td>
                       <td>{show.attendance.toLocaleString()}</td>
