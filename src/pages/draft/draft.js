30e7af445f9452ace66e138fd8148b8da9dc1bcd
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-16 03:51:43 +0000

    no message

--- src/pages/draft/draft.js
+++ src/pages/draft/draft.js
@@ -39,24 +39,31 @@ class DraftPage extends React.Component {
           <div className="inpage-content">
             <div className="draft no-select">
               <div className="row">
-                {this.props.brands.sort((prev, next) => prev.sequence > next.sequence ? 1 : -1).map((brand, key) => {
-                  let wrestlers = this.props.wrestlers
-                    .filter(wrestler => (brand.default === true && wrestler.brand === "") || wrestler.brand === brand.name)
-                    .sort((a, b) => a.rating < b.rating ? 1 : -1)
-                  return (
-                    <div
-                      key={brand.id}
-                      className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                      <Brand
-                        model={brand}
-                        canDragAndDrop={true}
-                        wrestlers={wrestlers}
-                        byPassBrandFilter={brand.default}
-                        showBrandLogo={true}
-                      />
+                <div className="col-xs-3">
+                draft
+                </div>
+                <div className="col-xs-9">
+                  <div className="row">
+                    {this.props.brands.sort((prev, next) => prev.sequence > next.sequence ? 1 : -1).map((brand, key) => {
+                      let wrestlers = this.props.wrestlers
+                        .filter(wrestler => (brand.default === true && wrestler.brand === "") || wrestler.brand === brand.name)
+                        .sort((a, b) => a.rating < b.rating ? 1 : -1)
+                      return (
+                        <div
+                          key={brand.id}
+                          className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                          <Brand
+                            model={brand}
+                            canDragAndDrop={true}
+                            wrestlers={wrestlers}
+                            byPassBrandFilter={brand.default}
+                            showBrandLogo={true}
+                          />
+                        </div>
+                      )
+                    })}
                     </div>
-                  )
-                })}
+                  </div>
               </div>
             </div>
           </div>
