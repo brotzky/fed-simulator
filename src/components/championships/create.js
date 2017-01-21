137e0ffce959ed31f051623502f83ae4e314ea6b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-21 23:25:48 +0000

    First draft of the belt

--- src/components/championships/create.js
+++ src/components/championships/create.js
@@ -1,8 +1,25 @@
 import React from "react"
+import "./stylesheets/create"
 
 export default class CreateChampionship extends React.Component {
 
   displayName = "CreateChampionship"
+
+  static propTypes = {
+    strapBackgroundColor: React.PropTypes.string,
+    platShape: React.PropTypes.string,
+    plateBackgroundColor: React.PropTypes.string,
+    plateColor: React.PropTypes.string,
+    championsName: React.PropTypes.string,
+  }
+
+  static defaultProps = {
+    platShape: "circle",
+    strapBackgroundColor: "#000",
+    plateBackgroundColor: "gold",
+    plateColor: "black",
+    championsName: "John Doe",
+  }
 
   render() {
     const strapStyle= {
@@ -15,23 +32,25 @@ export default class CreateChampionship extends React.Component {
     }
     return (
       <div className="belt">
-        <span className="strap"
-          style={strapStyle}>
-          <span className="belt__strap--right">
-            <span className="plate"></span>
-            <span className="buttons"></span>
-          </span>
-          <span className="belt__strap--center">
+        <span className="strap">
+          <span style={strapStyle}
+            className="belt__strap belt__strap--side">
             <span className="plate"
+              style={plateStyle}>&nbsp;</span>
+          </span>
+          <span style={strapStyle}
+            className="belt__strap belt__strap--center">
+            <span className="plate circle"
               style={plateStyle}>
               <span className="plate__nameplate">
-                {this.props.name}
+                {this.props.championsName}
               </span>
             </span>
           </span>
-          <span className="belt__strap--right">
-            <span className="plate"></span>
-            <span className="buttons"></span>
+          <span style={strapStyle}
+            className="belt__strap belt__strap--side">
+            <span className="plate"
+              style={plateStyle}>&nbsp;</span>
           </span>
         </span>
       </div>
