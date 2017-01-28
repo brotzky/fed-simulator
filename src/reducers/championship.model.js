70f9ebdb90db83c42d2ab157bcdc5b109b125e53
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-28 16:53:50 +0000

    Default Championship shouldn’t have an opinion on its name

--- src/reducers/championship.model.js
+++ src/reducers/championship.model.js
@@ -3,20 +3,21 @@ import { hashCode } from "../helpers/hash"
 const Model = Backbone.Model.extend({
   defaults: {
     id: hashCode(new Date().toString()),
-    name: "Default",
+    name: "",
     male: true,
+    brand: "Default",
     canMoveBrands: true,
     tag: false,
     sequence: 0,
     changes: 0,
     wrestlers: [],
     centerStrapShape: "circle",
-    centerPlateShape: "square",
-    centerPlateColor: "gold",
+    centerPlateShape: "circle",
+    centerPlateColor: "black",
     sideplateBackgroundColor: "gold",
     strapBackgroundColor: "black",
     sideplateShape: "rectangle",
-    centerPlateOverflow: true,
+    centerPlateOverflow: false,
   },
 })
 
