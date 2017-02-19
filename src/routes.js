dd80e3f6016acf348b024ff6a9b1daa8cf248bcb
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-02-19 21:34:57 +0000

    Better analytics tracking

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 import React from "react"
 import Page from "./components/page/page"
 import { IndexRoute, Route } from "react-router"
+import ga from "ga-react-router"
+
+const onChange = (previousRoute, nextRoute) => {
+  if (ga && nextRoute.location && nextRoute.location.pathname[0] != "/") {
+    ga('send', 'pageview', nextRoute.location.pathname)
+  }
+}
 
 export default () => {
   return (
     <Route
       path="/"
+      onChange={onChange}
       component={Page}>
       <IndexRoute
         getComponent={(nextState, callback) => {
