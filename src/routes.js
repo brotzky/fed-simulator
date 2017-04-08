23dea91a43ed0a23de0ac63f2fb9b8a4699a9ff0
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-08 18:34:14 +0700

    Finals for this codebase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 import React from "react"
 import Page from "./components/page/page"
 import { IndexRoute, Route } from "react-router"
 
 const onChange = (previousRoute, nextRoute) => {
-  if (ga && nextRoute.location && nextRoute.location.pathname[0] != "/") {
+  if (ga && nextRoute.location && nextRoute.location.pathname[0] !== "/") {
     ga('send', 'pageview', nextRoute.location.pathname)
   }
 }
