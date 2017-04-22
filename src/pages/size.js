5f662fffdb98dad1d328ee845182ab0367a889cc
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-22 20:35:59 +0100

    Sizes

--- src/pages/size.js
+++ src/pages/size.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import defaultOptions from './size.options.json'
+import './stylesheets/size.scss'
 
-const Size = () => {
+const Size = ({options = defaultOptions,}) => {
   return (
     <section className="page size">
-      <h1>Size</h1>
+      <h1>How big are you?!</h1>
+      <div className="row options">
+        {options.map(option => {
+          return (
+            <div className="col-xs-3 option" key={option.id}>
+              <h3>{option.name}</h3>
+              <p>{option.size}</p>
+            </div>
+          )
+        })}
+      </div>
     </section>
   )
 }
