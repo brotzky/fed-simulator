65d02e4704d02205f1eefab7709bbc1bf0eaf8f0
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-24 18:05:25 +0100

    Revert "Works?"
    This reverts commit 30fbedcf713553773222189601282c3612a2d799.

--- scripts/utils/getProcessForPort.js
+++ scripts/utils/getProcessForPort.js
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 'use strict';
 
 var chalk = require('chalk');
@@ -21,10 +12,6 @@ var execOptions = {
     'ignore', //stderr
   ],
 };
-
-function isProcessAReactApp(processCommand) {
-  return /^node .*react-scripts\/scripts\/start\.js\s?$/.test(processCommand);
-}
 
 function getProcessIdOnPort(port) {
   return execSync(
@@ -49,12 +36,7 @@ function getProcessCommand(processId, processDirectory) {
     execOptions
   );
 
-  if (isProcessAReactApp(command)) {
-    const packageName = getPackageNameInDirectory(processDirectory);
-    return packageName ? packageName + '\n' : command;
-  } else {
-    return command;
-  }
+  return command;
 }
 
 function getDirectoryOfProcessById(processId) {
--- scripts/utils/prompt.js
+++ scripts/utils/prompt.js
@@ -1,24 +1,11 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-'use strict';
-
 var rl = require('readline');
 
 // Convention: "no" should be the conservative choice.
 // If you mistype the answer, we'll always take it as a "no".
 // You can control the behavior on <Enter> with `isYesDefault`.
-function prompt(question, isYesDefault) {
+module.exports = function (question, isYesDefault) {
   if (typeof isYesDefault !== 'boolean') {
-    throw new Error(
-      'Provide explicit boolean isYesDefault as second argument.'
-    );
+    throw new Error('Provide explicit boolean isYesDefault as second argument.');
   }
   return new Promise(resolve => {
     var rlInterface = rl.createInterface({
@@ -41,6 +28,4 @@ function prompt(question, isYesDefault) {
       return resolve(isYes);
     });
   });
-}
-
-module.exports = prompt;
+};
