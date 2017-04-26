6a94a2e7eaefe09d47f1c92d76e6803b6005dd5d
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-26 22:59:05 +0100

    Calendar design

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import './stylesheets/calendar'
 
 class CalendarPage extends Component {
   displayName = 'ChampionsPage'
