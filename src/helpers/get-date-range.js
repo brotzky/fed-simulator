833d8d50c3f510e4b7b9a6d434d55409e22a3661
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-27 00:42:36 +0100

    Random

--- src/helpers/get-date-range.js
+++ src/helpers/get-date-range.js
@@ -5,7 +5,7 @@ export function getDateRange(startDate, stopDate) {
   let currentDate = moment(startDate)
   stopDate = moment(stopDate)
   while (currentDate <= stopDate) {
-    dateArray.push(moment(currentDate).format('YYYY-MM-DD'))
+    dateArray.push({date: moment(currentDate).format('YYYY-MM-DD'),})
     currentDate = moment(currentDate).add(1, 'days')
   }
   return dateArray
--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -6,23 +6,46 @@ import moment from 'moment'
 
 import {YEAR_FORMAT, DATE_FORMAT} from '../constants/calendar'
 
+const DEFAULT_SHOWS = []
+
 class CalendarPage extends Component {
   displayName = 'ChampionsPage'
 
   state = {
-    range: [],
+    rangeOfDates: [],
+    showsForTheWeek: [],
   }
 
   componentDidMount() {
-    let from = moment(this.props.federation.current_date).format()
-    let to = moment(this.props.federation.current_date).add(7, 'days').format()
+    const {shows, federation,} = this.props
 
-    console.log(from)
-    console.log(to)
+    let from = moment(federation.current_date).format()
+    let to = moment(federation.current_date).add(7, 'days').format()
+    let dateNumber = moment(federation.current_date).format('MM')
+    let rangeOfDates = getDateRange(from, to)
+
+    let weeklyShows = shows.filter(show => show.frequency === 'weekly')
+    let dailyShows = shows.filter(show => show.frequency === 'daily')
+    let monthlyShows = dateNumber > 25
+      ? shows.filter(show => show.frequency === 'monthly')
+      : DEFAULT_SHOWS
+
+    let showsForTheWeek = []
+      .concat(dailyShows.slice(0, 2))
+      .concat(weeklyShows.slice(0, 2))
+      .concat(monthlyShows.slice(0, 1))
 
     this.setState({
-      range: getDateRange(from, to),
+      rangeOfDates,
+      showsForTheWeek,
     })
+  }
+
+  _fetchRandomShow = () => {
+    const length = this.state.showsForTheWeek.length
+    const showKey = [Math.floor(Math.random() * length),]
+
+    return this.state.showsForTheWeek[showKey]
   }
 
   render() {
@@ -35,8 +58,16 @@ class CalendarPage extends Component {
         <h3 style={style}>
           Date: {moment(this.props.federation.current_date).format(YEAR_FORMAT)}
         </h3>
-        {this.state.range.map((date, key) => {
-          return <div key={key}>{moment(date).format(DATE_FORMAT)}</div>
+        {this.state.rangeOfDates.map((date, key) => {
+          let show = this._fetchRandomShow(this.state.showsForTheWeek)
+          return (
+            <div key={key}>
+              {moment(date.date).format(DATE_FORMAT)}
+              <If condition={show}>
+                : {show.name}
+              </If>
+            </div>
+          )
         })}
       </section>
     )
