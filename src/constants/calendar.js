8f707ea3060a22f8dd7e892bd8d381fe53c7c683
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-26 23:45:44 +0100

    Date ranges

---
+++ src/constants/calendar.js
@@ -0,0 +1,2 @@
+export const YEAR_FORMAT = 'MMMM Do YYYY'
+export const DATE_FORMAT = 'MMMM Do'
--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,35 +1,43 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import './stylesheets/calendar'
+import {getDateRange} from '../helpers/get-date-range'
+import moment from 'moment'
+
+import {YEAR_FORMAT, DATE_FORMAT} from '../constants/calendar'
 
 class CalendarPage extends Component {
   displayName = 'ChampionsPage'
 
+  state = {
+    range: [],
+  }
+
+  componentDidMount() {
+    let from = moment(this.props.federation.current_date).format()
+    let to = moment(this.props.federation.current_date).add(7, 'days').format()
+
+    console.log(from)
+    console.log(to)
+
+    this.setState({
+      range: getDateRange(from, to),
+    })
+  }
+
   render() {
+    const style = {
+      color: '#' + ((Math.random() * 0xffffff) << 0).toString(16),
+    }
     return (
       <section className="page calendar">
         <h1>Calendar</h1>
-        <ul>
-          {this.props.shows.map((show, key) => {
-            return (
-              <li key={key}>
-                {show.name}
-                :
-                {' '}
-                {show.frequency}
-                :
-                {' '}
-                {show.size}
-                :
-                {' '}
-                {show.max_cost}
-                :
-                {' '}
-                {show.max_gross}
-              </li>
-            )
-          })}
-        </ul>
+        <h3 style={style}>
+          Date: {moment(this.props.federation.current_date).format(YEAR_FORMAT)}
+        </h3>
+        {this.state.range.map((date, key) => {
+          return <div key={key}>{moment(date).format(DATE_FORMAT)}</div>
+        })}
       </section>
     )
   }
@@ -37,4 +45,11 @@ class CalendarPage extends Component {
 
 export default connect(state => ({
   shows: state.shows,
+  federation: state.federation,
 }))(CalendarPage)
+
+// {
+//   this.state.range.map(date => {
+//     return <div>{date}</div>
+//   })
+// }
