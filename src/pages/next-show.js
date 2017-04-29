b4c7874aef4efebe756ed6f65d52e968a7178387
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-04-29 02:45:41 +0100

    Before the storm

--- src/pages/next-show.js
+++ src/pages/next-show.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
-const NextShow = () => {
-  return (
-    <section className="page next-show">
-      <h1>Next Show</h1>
-    </section>
-  )
+class NextShow extends Component {
+  render() {
+    return (
+      <section className="page next-show">
+        <h1>
+          Next show
+        </h1>
+        <div className="row top-xs">
+          <div className="col-xs-12 col-lg-6">
+            <div className="box">
+              <h2>
+                Roster
+              </h2>
+              <ul>
+                {this.props.roster.map(wrestler => {
+                  return (
+                    <li key={wrestler.name}>
+                      {wrestler.name}
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
 
-export default NextShow
+export default connect(state => ({
+  roster: state.roster,
+}))(NextShow)
