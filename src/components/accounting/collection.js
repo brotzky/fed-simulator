840a77847a4692378509fb3a69a12fc14a520616
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-05-01 00:57:08 +0100

    Pass data around

--- src/components/accounting/collection.js
+++ src/components/accounting/collection.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
-const AccountingCollection = ({events,}) => {
+const AccountingCollection = ({events, totalCost, federationCash,}) => {
   return (
-    <ul>
-      {events.map((event, key) => {
-        return (
-          <li key={key}>
-            {event.name} {event.cost}
-          </li>
-        )
-      })}
-    </ul>
+    <div>
+      <div>
+        Federation cash available: {federationCash}
+      </div>
+      <hr />
+      <ul>
+        {events.map((event, key) => {
+          return (
+            <li key={key}>
+              {event.name} {event.cost}
+            </li>
+          )
+        })}
+      </ul>
+      <hr />
+      <div>
+        Total Cost = {totalCost}
+      </div>
+    </div>
   )
 }
 
--- src/components/accounting/container.js
+++ src/components/accounting/container.js
@@ -2,13 +2,29 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import Collection from './collection'
 
+function amount(item) {
+  return item.cost
+}
+
+function sum(current, next) {
+  return current + next
+}
+
 class AccountingContainer extends Component {
   render() {
     const events = this.props.events.filter(event => event.cost > 0)
-    return <Collection events={events} />
+    const totalCost = events.map(amount).reduce(sum)
+    return (
+      <Collection
+        events={events}
+        totalCost={totalCost}
+        federationCash={this.props.federation.cash}
+      />
+    )
   }
 }
 
 export default connect(state => ({
   events: state.events,
+  federation: state.federation,
 }))(AccountingContainer)
