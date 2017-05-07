d9b99e2d1d283f12130424090e9302add4702e6b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-05-07 19:56:36 +0100

    group live shows by size

--- src/components/accounting/collection.js
+++ src/components/accounting/collection.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import groupBy from 'lodash.groupby'
 import {nFormatter} from '../../helpers/nFormatter'
 
 const AccountingCollection = ({liveShows, totalCost, federationCash,}) => {
+  liveShows = groupBy(liveShows, 'size')
   return (
     <div>
       <div>
         Cash available: {nFormatter(federationCash)}
       </div>
       <hr />
-      <ul>
-        {liveShows.map((event, key) => {
-          return (
-            <li key={key}>
-              {event.name} {nFormatter(event.cost)}
-            </li>
-          )
-        })}
-      </ul>
+      {Object.keys(liveShows).map((key, shows) => {
+        return (
+          <div key={key}>
+            <h4>Size: {key}</h4>
+            <ul>
+              {liveShows[key].map((show, key) => {
+                return (
+                  <li key={key}>
+                    {show.name} {nFormatter(show.cost)}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
       <hr />
       <div>
         Total Cost = {nFormatter(totalCost)}
