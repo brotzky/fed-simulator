5e3f3dc16fbeca6a7f3f6a95a62342aed73ecb50
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-05-08 00:41:56 +0100

    Remove excess

--- src/components/generate-random/index.js
+++ src/components/generate-random/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const GenerateRandom = ({onClick,}) => {
-  return (
-    <i className="fa fa-random" aria-hidden="true" onClick={onClick}>
-      Generate
-    </i>
-  )
+  return <i className="fa fa-random" aria-hidden="true" onClick={onClick} />
 }
 
 export default GenerateRandom
