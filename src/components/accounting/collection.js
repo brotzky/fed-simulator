c3fa76cf3aec3261edd6a7c18115f020b256bb44
Aaron Lote <aaron.lote@gamesys.co.uk>
2017-05-08 13:30:36 +0100

    Streamline accounting with flexboxgrid. Lunch over.

--- src/components/accounting/collection.js
+++ src/components/accounting/collection.js
@@ -13,6 +13,25 @@ const AccountingCollection = ({
   federationCash,
 }) => {
   liveShows = groupBy(liveShows, 'size')
+	const heading = (
+		<div className="row around-xs heading">
+			<div className="col-xs-2">
+					<div className="box">
+							Live Show
+					</div>
+			</div>
+			<div className="col-xs-2">
+					<div className="box">
+							Cost
+					</div>
+			</div>
+			<div className="col-xs-2">
+					<div className="box">
+							Gross
+					</div>
+			</div>
+		</div>
+	)
   return (
     <div className="accounting">
       <div className="total">
@@ -24,44 +43,38 @@ const AccountingCollection = ({
         return (
           <div className="accounting__collection" key={index}>
             <h4 className="accounting__header">Size: {index}</h4>
-            <ul className="accounting__list">
-              {liveShows[index].map((show, key) => {
-                return (
-                  <li className="item" key={key}>
-                    <table className="table">
-                      <tbody>
-                        <tr>
-                          <td colSpan="2">
-                            <If condition={!isComplete}>
-                              <span
-                                className="item__delete fa fa-trash red"
-                                data-date={show.date}
-                                onClick={onClickDelete}
-                              />
-                              &nbsp;
-                            </If>
-                            {show.name}
-                            <span className="item__cost">
-                              {nFormatter(show.cost)}
-                            </span>
-                          </td>
-                        </tr>
-                        <If condition={show.gross > 0}>
-                          <tr>
-                            <td className="gross" colSpan="2">
-                              Gross
-                              <span className="item__cost">
-                                {nFormatter(show.gross)}
-                              </span>
-                            </td>
-                          </tr>
-                        </If>
-                      </tbody>
-                    </table>
-                  </li>
-                )
-              })}
-            </ul>
+            {heading}
+            {liveShows[index].map((show, key) => {
+              return (
+								<div key={key} className="row around-xs">
+									<div className="col-xs-2">
+										<div className="box">
+											<If condition={!isComplete}>
+												<span
+													className="item__delete fa fa-trash red"
+													data-date={show.date}
+													onClick={onClickDelete}
+												/>
+												&nbsp;
+											</If>
+											{show.name}
+										</div>
+									</div>
+									<div className="col-xs-2">
+										<div className="box cost">
+											{nFormatter(show.cost)}
+										</div>
+									</div>
+									<div className="col-xs-2">
+										<div className="box gross">
+											<If condition={show.gross > 0}>
+												{nFormatter(show.gross)}
+											</If>
+										</div>
+									</div>
+								</div>
+              )
+            })}
           </div>
         )
       })}
