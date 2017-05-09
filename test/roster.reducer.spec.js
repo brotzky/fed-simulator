7032dda19f3e3dd11a753408603b3eaf890e28e2
Aaron Lote <aaron.lote@gamesys.co.uk>
2017-05-09 11:21:11 +0100

    Working roster reducer test

---
+++ test/roster.reducer.spec.js
@@ -0,0 +1,20 @@
+import reducer from '../src/reducers/version'
+import * as types from '../src/actions/types'
+
+const action = {
+        type: types.CHECK_VERSION,
+}
+
+describe('version reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, action)
+    ).to.equal(5)
+  })
+
+        it('should return the updated state', () => {
+                expect(
+                        reducer(6, action)
+                ).to.equal(6)
+        })
+})
--- test/version.reducer.spec.js
+++ test/version.reducer.spec.js
@@ -1,6 +1,15 @@
-import reducer from '../src/reducers/version'
+import reducer from '../src/reducers/roster'
 import * as types from '../src/actions/types'
 
+const defaultMaleWrestler = {
+	name: "man",
+	male: true,
+}
+const defaultFemaleWrestler = {
+        name: "woman",
+        male: false,
+}
+const defaultCollection = [defaultMaleWrestler, defaultFemaleWrestler,]
 const action = {
 	type: types.CHECK_VERSION,
 }
@@ -9,12 +18,12 @@ describe('version reducer', () => {
   it('should return the initial state', () => {
     expect(
       reducer(undefined, action)
-    ).to.equal(5)
+    ).to.be.empty
   })
 
 	it('should return the updated state', () => {
 		expect(
-			reducer(6, action)
-		).to.equal(6)
+			reducer(defaultCollection, action).length
+		).to.equal(2)
 	})
 })
