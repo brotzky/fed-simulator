665c274f6f5fb8823e6e5499668af7af17ebdb36
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-05-09 08:08:40 +0100

    Collection changes to be more compassable

--- src/components/accounting/collection.js
+++ src/components/accounting/collection.js
@@ -1,8 +1,47 @@
-import React from 'react'
-import groupBy from 'lodash.groupby'
-import {nFormatter} from '../../helpers/nFormatter'
+import React from "react"
+import groupBy from "lodash.groupby"
+import { nFormatter } from "../../helpers/nFormatter"
 
 const noop = () => {}
+
+const liveShowName = (
+  isComplete = false,
+  name = "",
+  date = Date(),
+  onClickDelete = noop
+) => (
+  <span>
+    <If condition={!isComplete}>
+      <span
+        className="item__delete fa fa-trash red"
+        data-date={date}
+        onClick={onClickDelete}
+      />
+      &nbsp;
+    </If>
+    {name}
+  </span>
+)
+
+const heading = ({ first = "", second = "", third = "", }) => (
+  <div className="row around-xs heading">
+    <div className="col-xs-6">
+      <div className="box">
+        {first}
+      </div>
+    </div>
+    <div className="col-xs-3">
+      <div className="box cost">
+        {second}
+      </div>
+    </div>
+    <div className="col-xs-3">
+      <div className="box gross">
+        {third}
+      </div>
+    </div>
+  </div>
+)
 
 const AccountingCollection = ({
   liveShows = [],
@@ -12,88 +51,48 @@ const AccountingCollection = ({
   onClickDelete = noop,
   federationCash,
 }) => {
-  liveShows = groupBy(liveShows, 'size')
-	const heading = (
-		<div className="row around-xs heading">
-			<div className="col-xs-2">
-					<div className="box">
-							Live Show
-					</div>
-			</div>
-			<div className="col-xs-2">
-					<div className="box">
-							Cost
-					</div>
-			</div>
-			<div className="col-xs-2">
-					<div className="box">
-							Gross
-					</div>
-			</div>
-		</div>
-	)
+  liveShows = groupBy(liveShows, "size")
+  federationCash = nFormatter(federationCash)
   return (
     <div className="accounting">
-      <div className="total">
-        <span className="total__title">Cash available</span>
-        <span className="total__cost">{nFormatter(federationCash)}</span>
-      </div>
+      {heading({
+        first: "Cash Available",
+        second: "",
+        third: federationCash,
+      })}
       <hr />
       {Object.keys(liveShows).map(index => {
         return (
           <div className="accounting__collection" key={index}>
-            <h4 className="accounting__header">Size: {index}</h4>
-            {heading}
-            {liveShows[index].map((show, key) => {
-              return (
-								<div key={key} className="row around-xs">
-									<div className="col-xs-2">
-										<div className="box">
-											<If condition={!isComplete}>
-												<span
-													className="item__delete fa fa-trash red"
-													data-date={show.date}
-													onClick={onClickDelete}
-												/>
-												&nbsp;
-											</If>
-											{show.name}
-										</div>
-									</div>
-									<div className="col-xs-2">
-										<div className="box cost">
-											{nFormatter(show.cost)}
-										</div>
-									</div>
-									<div className="col-xs-2">
-										<div className="box gross">
-											<If condition={show.gross > 0}>
-												{nFormatter(show.gross)}
-											</If>
-										</div>
-									</div>
-								</div>
-              )
+            {heading({
+              first: `Size: ${index}`,
+              second: "Cost",
+              third: "Gross",
+            })}
+            {liveShows[index].map(show => {
+              const { name, date, } = show
+              return heading({
+                first: liveShowName(isComplete, name, date, onClickDelete),
+                second: nFormatter(show.cost),
+                third: show.gross > 0 ? nFormatter(show.gross) : "",
+              })
             })}
           </div>
         )
       })}
-      <hr />
-      <div className="total">
-        <span className="total__title">Total Cost</span>
-        <span className="total__cost total">{nFormatter(totalCost)}</span>
-      </div>
+      <br />
+      {heading({
+        first: "Totals",
+        second: nFormatter(totalCost),
+        third: nFormatter(totalGross),
+      })}
       <If condition={isComplete}>
-        <div className="total">
-          <span className="total__title">Total Gross</span>
-          <span className="total__cost gross">{nFormatter(totalGross)}</span>
-        </div>
-        <div className="total">
-          <span className="total__title">Total Profit</span>
-          <span className="total__cost profit">
-            {nFormatter(totalGross - totalCost)}
-          </span>
-        </div>
+        <br />
+        {heading({
+          first: "Profit",
+          second: "",
+          third: nFormatter(totalGross - totalCost),
+        })}
       </If>
     </div>
   )
