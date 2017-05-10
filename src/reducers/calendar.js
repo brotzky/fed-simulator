009bd24aaec02f1bf78ecc78094a724de8cd3eec
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-05-10 23:10:45 +0100

    Calendar tests

--- src/reducers/calendar.js
+++ src/reducers/calendar.js
@@ -1,9 +1,9 @@
-import moment from 'moment'
+import moment from "moment"
 
-import showsOptions from '../constants/shows.options.json'
-import {getRandomArbitrary} from '../helpers/math.js'
-import {getDateRange} from '../helpers/get-date-range'
-import LiveShowModel from './liveShow.model'
+import showsOptions from "../constants/shows.options.json"
+import { getRandomArbitrary } from "../helpers/math.js"
+import { getDateRange } from "../helpers/get-date-range"
+import Model from "./liveShow.model"
 
 const firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1)
 const lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth() + 1, 0)
@@ -23,42 +23,42 @@ const defaultState = {
 export default (state = defaultState, action) => {
   let newState = JSON.parse(JSON.stringify(state))
   switch (action.type) {
-    case 'RESET':
-    case 'RESET_CALENDAR':
+    case "RESET":
+    case "RESET_CALENDAR":
       newState = defaultState
       break
-    case 'DELETE_CALENDAR_LIVESHOW':
+    case "DELETE_CALENDAR_LIVESHOW":
       newState.collection = newState.collection.map(liveShow => {
         if (liveShow.date === action.payload) {
-          liveShow.name = ''
+          liveShow.name = ""
           liveShow.cost = 0
           liveShow.showId = false
         }
         return liveShow
       })
       break
-    case 'GENERATE_CALENDAR_LIVESHOWS':
+    case "GENERATE_CALENDAR_LIVESHOWS":
       newState.collection = []
       newState.inProgress = true
       newState.dateRange.forEach(date => {
-        newState.collection.push(new LiveShowModel({date: date,}).toJSON())
+        newState.collection.push(new Model({ date: date, }).toJSON())
       })
       break
-    case 'START_CALENDAR_MONTH':
+    case "START_CALENDAR_MONTH":
       newState.collection = []
       newState.inProgress = true
       newState.isComplete = false
-      newState.firstDay = moment(newState.firstDay).add(1, 'M').toDate()
-      newState.lastDay = moment(newState.firstDay).endOf('month').toDate()
+      newState.firstDay = moment(newState.firstDay).add(1, "M").toDate()
+      newState.lastDay = moment(newState.firstDay).endOf("month").toDate()
       newState.currentDay = newState.firstDay
       newState.dateRange.forEach(date => {
-        newState.collection.push(new LiveShowModel({date: date,}).toJSON())
+        newState.collection.push(new Model({ date: date, }).toJSON())
       })
       break
-    case 'UPDATE_CALENDAR_LIVESHOWS':
+    case "UPDATE_CALENDAR_LIVESHOWS":
       newState.collection = action.payload
       break
-    case 'SIMULATE_CALENDAR_LIVESHOWS':
+    case "SIMULATE_CALENDAR_LIVESHOWS":
       newState.collection = newState.collection.map(liveShow => {
         const options = showsOptions.find(
           options => options.size === liveShow.size
@@ -74,24 +74,29 @@ export default (state = defaultState, action) => {
       newState.isComplete = true
       newState.currentDay = newState.lastDay
       break
-    case 'UPDATE_CALENDAR_LIVESHOW':
-      const {show, dateIndex,} = action.payload
+    case "UPDATE_CALENDAR_LIVESHOW":
+      const { show, dateIndex, } = action.payload
       const options = showsOptions.find(options => options.size === show.size)
 
-      newState.collection[dateIndex].name = show.name
-      newState.collection[dateIndex].size = show.size
-      newState.collection[dateIndex].showId = show.id
-      newState.collection[dateIndex].cost = getRandomArbitrary(
-        options.min_cost,
-        options.max_cost
-      )
+      if (newState.collection[dateIndex]) {
+        newState.collection[dateIndex].name = show.name
+        newState.collection[dateIndex].size = show.size
+        newState.collection[dateIndex].showId = show.id
+        newState.collection[dateIndex].cost = getRandomArbitrary(
+          options.min_cost,
+          options.max_cost
+        )
+      }
       break
-    case 'SIMULATE_EVENT':
+    case "SIMULATE_EVENT":
       break
     default:
       break
   }
 
   newState.dateRange = getDateRange(newState.firstDay, newState.lastDay)
+  newState.collection = newState.collection.map(liveShow =>
+    new Model(liveShow).toJSON()
+  )
   return newState
 }
--- test/calendar.reducer.spec.js
+++ test/calendar.reducer.spec.js
@@ -1,74 +1,108 @@
-import reducer from "../src/reducers/championships"
+import reducer from "../src/reducers/calendar"
 import * as types from "../src/actions/types"
-import Model from "../src/reducers/championship.model"
+import Model from "../src/reducers/liveShow.model"
 
 const defaultModel = new Model()
-const defaultMale = {
-  name: "man",
-  male: true,
+const firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1)
+const lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth() + 1, 0)
+const { isComplete, inProgress, } = false
+
+const defaultState = {
+  firstDay,
+  lastDay,
+  currentDay: firstDay,
+  inProgress,
+  isComplete,
+  collection: [
+    {
+      name: "first",
+      size: "xs",
+      firstDay,
+      lastDay,
+    },
+    {
+      name: "first",
+      size: "xs",
+    },
+  ],
 }
-const defaultFemale = {
-  name: "woman",
-  male: false,
-}
-const defaultCollection = [defaultMale, defaultFemale,]
 const action = {
-  type: types.UPDATE_CHAMPIONS,
-  payload: [],
+  type: "",
+  payload: "",
 }
 
-describe("given a championships reducer", () => {
-  let championshipsReducer
+describe("given a calendars reducer", () => {
+  let calendarReducer
 
-  before(() => (championshipsReducer = reducer(undefined, action)))
+  before(() => (calendarReducer = reducer(undefined, action)))
 
   it("should return the initial state", () => {
-    expect(championshipsReducer).to.be.empty
+    expect(calendarReducer).to.be.empty
   })
 
   describe("and a collection is passed in", () => {
     before(() => {
-      action.payload = defaultCollection
-      championshipsReducer = reducer(undefined, action)
+      action.payload = defaultState.collection
+      action.type = types.UPDATE_CALENDAR_LIVESHOWS
+      calendarReducer = reducer(undefined, action)
     })
 
     it("should now have two in the collection", () => {
-      expect(championshipsReducer.length).to.equal(2)
-    })
-  })
-
-  describe("and the championships has been passed through the model", () => {
-    let championshipsReducer
-
-    before(() => {
-      action.payload = defaultCollection
-      championshipsReducer = reducer(undefined, action)
+      expect(calendarReducer.collection.length).to.equal(2)
     })
 
-    it("should have a male championship with an id", () => {
-      expect(championshipsReducer[0].male).to.equal(true)
-      expect(championshipsReducer[0].id).to.not.be.empty
+    it("should have a first day with an id", () => {
+      expect(calendarReducer.collection[0].id).to.not.be.empty
     })
 
     it("should have a female championship with an id", () => {
-      expect(championshipsReducer[1].male).to.equal(false)
-      expect(championshipsReducer[1].id).to.not.be.empty
+      expect(calendarReducer.collection[1].id).to.not.be.empty
     })
 
     it("should have correct stats", () => {
-      expect(championshipsReducer[0].losses).to.equal(
-        defaultModel.toJSON().losses
+      expect(calendarReducer.collection[0].gross).to.equal(
+        defaultModel.toJSON().gross
       )
+    })
+
+    describe("and a live show is updated", () => {
+      let calendarReducer
+
+      before(() => {
+        action.payload = defaultState.collection
+        action.type = types.UPDATE_CALENDAR_LIVESHOWS
+        calendarReducer = reducer(undefined, action)
+      })
+
+      it("should expect the model to update", () => {
+        let show = calendarReducer.collection[0]
+        show.name = "Test"
+        show.size = "lg"
+
+        action.type = types.UPDATE_CALENDAR_LIVESHOW
+        action.payload = {
+          show,
+          dateIndex: 0,
+        }
+
+        calendarReducer = reducer(calendarReducer, action)
+
+        const matchingShow = calendarReducer.collection[0]
+
+        expect(matchingShow.name).to.equal("Test")
+        expect(matchingShow.size).to.equal("lg")
+      })
     })
 
     describe("and reset is called", () => {
       before(() => {
         action.type = types.RESET
-        championshipsReducer = reducer(undefined, action)
+        action.payload = false
+        calendarReducer = reducer(undefined, action)
       })
 
-      it("should have NO wrestlers", () => {
-        expect(championshipsReducer.length).to.equal(0)
+      it("should have an empty collection", () => {
+        expect(calendarReducer.collection.length).to.equal(0)
       })
     })
   })
