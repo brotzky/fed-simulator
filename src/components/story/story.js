6a199a4e976d3946d642af6bc062fc392dfbf714
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-06-12 21:05:05 +0100

    Remove animation directly from story

--- src/components/story/story.js
+++ src/components/story/story.js
@@ -5,22 +5,20 @@ import "./story.scss"
 
 const Story = ({ story = [], }) => (
   <ul className="story">
-    <SlideUp duration=".5s">
-      <If condition={story.length > 0}>
-        {story.map(storyItem => {
-          const { id, attacker, move, defender, } = storyItem
+    <If condition={story.length > 0}>
+      {story.map(storyItem => {
+        const { id, attacker, move, defender, } = storyItem
 
-          return (
-            <li className="story__action" key={id}>
-              <strong>{attacker.name}</strong>
-              &nbsp;{move.name} for
-              &nbsp;{move.damage} damage on
-              <strong> {defender.name}</strong>
-            </li>
-          )
-        })}
-      </If>
-    </SlideUp>
+        return (
+          <li className="story__action" key={id}>
+            <strong>{attacker.name}</strong>
+            &nbsp;{move.name} for
+            &nbsp;{move.damage} damage on
+            <strong> {defender.name}</strong>
+          </li>
+        )
+      })}
+    </If>
   </ul>
 )
 
