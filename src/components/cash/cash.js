bcbeecc81aeba419005a0ad7ce8e8413460aa0c3
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-07-23 20:39:53 +0100

    Cleaner component

--- src/components/cash/cash.js
+++ src/components/cash/cash.js
@@ -4,10 +4,9 @@ import { formatCurrency } from "../../helpers/currency"
 
 import "./cash.scss"
 
-const Cash = ({ currency = "$", cash = 0, }) => (
+const Cash = ({ currency = "$", cash = 0, }) =>
   <span className="cash">
     {formatCurrency(currency, cash)}
   </span>
-)
 
 export default Cash
