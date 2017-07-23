8c2c6abd9d9a37fcc6b93c57e15b7d30aeaf95ff
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-07-23 20:39:08 +0100

    Cleaner button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,18 +4,15 @@ import "./button.scss"
 
 const noop = () => {}
 
-const Button = ({ value = "", onClick = noop, }) => {
-  return (
-    <div className="row">
-      <div className="col-xs-12">
-        <div className="box">
-          <button type="submit" className="btn" onClick={onClick}>
-            {value}
-          </button>
-        </div>
+const Button = ({ value = "", onClick = noop, }) =>
+  <div className="row">
+    <div className="col-xs-12">
+      <div className="box">
+        <button type="submit" className="btn" onClick={onClick}>
+          {value}
+        </button>
       </div>
     </div>
-  )
-}
+  </div>
 
 export default Button
