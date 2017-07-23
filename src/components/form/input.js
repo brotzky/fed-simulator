3486a0596385bcdda1774176679f3ce1009bfad3
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-07-23 20:39:24 +0100

    Simplyify the input component

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -8,30 +8,23 @@ const Input = ({
   name = "",
   onChange = noop,
   placeholder = "",
-}) => {
-  return (
-    <div className="row">
-      <div className="col-xs-12">
-        <div className="box">
-          <If condition={label.length > 0}>
-            <label htmlFor={name}>
-              {label}
-            </label>
-          </If>
-          <input
-            value={value}
-            id={name}
-            name={name}
-            onChange={onChange}
-            onKeyDown={onChange}
-            onKeyPress={onChange}
-            placeholder={placeholder}
-            type="text"
-          />
-        </div>
-      </div>
-    </div>
-  )
-}
+}) =>
+  <div>
+    <If condition={label.length > 0}>
+      <label htmlFor={name}>
+        {label}
+      </label>
+    </If>
+    <input
+      value={value}
+      id={name}
+      name={name}
+      onChange={onChange}
+      onKeyDown={onChange}
+      onKeyPress={onChange}
+      placeholder={placeholder}
+      type="text"
+    />
+  </div>
 
 export default Input
