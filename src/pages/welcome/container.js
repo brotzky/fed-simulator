f3651994f7542f390956d79fa55fae96bcf8f940
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-08-06 20:17:58 +0100

    Redirect to dashboard after generating the federation

--- src/pages/welcome/container.js
+++ src/pages/welcome/container.js
@@ -1,12 +1,26 @@
 import React from "react"
-import { compose } from "recompose"
+import { compose, lifecycle } from "recompose"
 import { connect } from "react-redux"
+import { withRouter } from "react-router"
 
 import WelcomePage from "./welcome"
 import { generateFederation } from "../../actions/game"
 
 export default compose(
-  connect(null, dispatch => ({
-    generateFederation: () => dispatch(generateFederation()),
-  }))
+  connect(
+    state => ({
+      started: state.game.started,
+    }),
+    dispatch => ({
+      generateFederation: () => dispatch(generateFederation()),
+    })
+  ),
+  withRouter,
+  lifecycle({
+    componentWillReceiveProps(props) {
+      if (props.started) {
+        props.router.push("/dashboard")
+      }
+    },
+  })
 )(WelcomePage)
