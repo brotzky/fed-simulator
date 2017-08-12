ea33916d08a901669d9114c8b35e5742de3aab9f
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-08-12 17:07:03 +0100

    Update button component to allow children over value

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,12 +4,12 @@ import "./button.scss"
 
 const noop = () => {}
 
-const Button = ({ value = "", onClick = noop, }) =>
+const Button = ({ value = "", children = "", onClick = noop, }) =>
   <div className="row">
     <div className="col-xs-12">
       <div className="box">
         <button type="submit" className="btn" onClick={onClick}>
-          {value}
+          {value ? value : children}
         </button>
       </div>
     </div>
