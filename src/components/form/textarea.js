edd18dcc9123c4fb6689d0fc059f83d4b1570874
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-08-12 17:33:19 +0100

    Styled textarea

--- src/components/form/textarea.js
+++ src/components/form/textarea.js
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from "react"
+
+import "./textarea.scss"
 
 const noop = () => {}
 
 const Textarea = ({
-  value = '',
-  label = '',
-  name = '',
+  value = "",
+  label = "",
+  name = "",
   onChange = noop,
-  placeholder = '',
+  placeholder = "",
   rows = 3,
 }) => {
   return (
-    <div className="row">
-      <div className="col-xs-12">
-        <div className="box">
-          <label htmlFor={name}>
-            {label}
-          </label>
-          <textarea
-            value={value}
-            id={name}
-            name={name}
-            onChange={onChange}
-            placeholder={placeholder}
-            rows={rows}
-            type="text"
-          />
-        </div>
-      </div>
+    <div className="box">
+      <label htmlFor={name}>
+        {label}
+      </label>
+      <textarea
+        value={value}
+        id={name}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        rows={rows}
+        type="text"
+      />
     </div>
   )
 }
