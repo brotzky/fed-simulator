89cc30010c91b756919d8b80b287a7f1d76c00f2
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-08-13 11:22:48 +0100

    Feature list on the front page

--- src/pages/welcome/welcome.js
+++ src/pages/welcome/welcome.js
@@ -13,7 +13,7 @@ const WelcomePage = ({ generateFederation = noop, }) => {
     <section className="page welcome">
       <HeaderOne>Welcome to Fed Simulator</HeaderOne>
       <div className="row">
-        <div className="col-xs-12 highlight pulse">
+        <div className="col-xs-12 highlight grow">
           <div className="box">
             <Link to="/name">
               I'll build this company from the ground up, dammit!
@@ -23,9 +23,22 @@ const WelcomePage = ({ generateFederation = noop, }) => {
       </div>
       <br />
       <div className="row">
-        <div className="col-xs-12 highlight pulse">
+        <div className="col-xs-12 highlight grow">
           <div className="box" onClick={generateFederation}>
             This is an invasion, I bought the company
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-xs-12 highlight shrink">
+          <div className="box">
+            <i className="icon fa fa-info-circle" />
+            <ul>
+              <li>Create and manage your roster</li>
+              <li>Create your own touring schedule</li>
+              <li>Randomly simulate matches</li>
+            </ul>
           </div>
         </div>
       </div>
