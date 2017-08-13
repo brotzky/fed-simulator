aad6d16b06b50a9b068f5404154ea0598a0a7292
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-08-13 15:40:14 +0100

    Improved branding

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,5 +1,5 @@
 import chromatism from "chromatism"
 
-export const sprectrum = chromatism.adjacent(20, 100, "#cdb403").hex
+export const sprectrum = chromatism.adjacent(10, 90, "#cdb403").hex
 export const defaultColors = ["#000000", "#4A4A4A", "#9B9B9B", "#FFFFFF",]
 export const colors = Object.assign([], sprectrum, defaultColors)
