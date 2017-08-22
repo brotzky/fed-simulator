6d7844cc1f4a3036ade5f315bfb1527b3c0fdf89
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-08-22 21:20:18 +0100

    Wrap input with style tag for brand page

--- src/pages/manage-brands/manage-brands.js
+++ src/pages/manage-brands/manage-brands.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 
 import HeaderOne from "../../components/h1/h1"
 import Brand from "../../components/brand/brand"
+import Input from "../../components/form/input"
 
 import "./manage-brands.scss"
 
@@ -13,7 +14,11 @@ const ManageBrandsPage = ({ brands = [], }) => {
       <div className="row">
         <div className="col-xs-12">
           {brands.map(brand => {
-            return <Brand key={brand.id} {...brand} />
+            return (
+              <div key={brand.id} style={brand.style}>
+                <Input style={brand.style} value={brand.name} />
+              </div>
+            )
           })}
         </div>
       </div>
