65195810b964c5fe05272277045c95f62d851291
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-09-05 21:49:35 +0100

    Remove some excess from the dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -14,39 +14,16 @@ import { COST_COLUMNS, RANKED_COLUMNS } from "../constants/ranking"
 
 import "./stylesheets/dashboard.scss"
 
-export const DashboardPage = ({
-  animations,
-  style,
-  expensiveWrestlers,
-  cheapWrestlers,
-  rankedMaleWrestlers,
-  rankedFemaleWrestlers,
-}) => {
+export const DashboardPage = ({ animations, style, expensiveWrestlers, cheapWrestlers, rankedMaleWrestlers, rankedFemaleWrestlers, }) => {
   const color = { color: style.color, }
   return (
     <section className="page dashboard zoom">
       <div className="row center-xs">
-        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-          <div className="box">
-            <Link style={color} to="/calendar">
-              <h3>
-                <i className="icon fa fa-calendar" /> Calendar
-              </h3>
-            </Link>
-          </div>
-        </div>
-        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+        <div className="col-xs-12">
           <div className="box">
             <h3>
               <Simulator />
             </h3>
-          </div>
-        </div>
-        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-          <div className="box">
-            <Link style={color} to="/create-a-match">
-              <h3>Create a Match</h3>
-            </Link>
           </div>
         </div>
       </div>
@@ -54,29 +31,14 @@ export const DashboardPage = ({
         <div className="col-xs-12 col-sm-6 col-lg-6">
           <SlideLeft iterations={Number(animations)} duration={ANIMATION_SPEED}>
             <div className="box">
-              <Ranking
-                style={style}
-                amountToShow={10}
-                rows={rankedMaleWrestlers}
-                columns={RANKED_COLUMNS}
-                title="Ranked Male Wrestlers"
-              />
+              <Ranking style={style} amountToShow={10} rows={rankedMaleWrestlers} columns={RANKED_COLUMNS} title="Ranked Male Wrestlers" />
             </div>
           </SlideLeft>
         </div>
         <div className="col-xs-12 col-sm-6 col-lg-6">
-          <SlideRight
-            iterations={Number(animations)}
-            duration={ANIMATION_SPEED}
-          >
+          <SlideRight iterations={Number(animations)} duration={ANIMATION_SPEED}>
             <div className="box">
-              <Ranking
-                style={style}
-                amountToShow={10}
-                rows={rankedFemaleWrestlers}
-                columns={RANKED_COLUMNS}
-                title="Ranked Female Wrestlers"
-              />
+              <Ranking style={style} amountToShow={10} rows={rankedFemaleWrestlers} columns={RANKED_COLUMNS} title="Ranked Female Wrestlers" />
             </div>
           </SlideRight>
         </div>
@@ -85,29 +47,14 @@ export const DashboardPage = ({
         <div className="col-xs-12 col-sm-6 col-lg-6">
           <SlideLeft iterations={Number(animations)} duration={ANIMATION_SPEED}>
             <div className="box">
-              <Ranking
-                style={style}
-                amountToShow={5}
-                rows={expensiveWrestlers}
-                columns={COST_COLUMNS}
-                title="Expensive Wrestlers"
-              />
+              <Ranking style={style} amountToShow={5} rows={expensiveWrestlers} columns={COST_COLUMNS} title="Expensive Wrestlers" />
             </div>
           </SlideLeft>
         </div>
         <div className="col-xs-12 col-sm-6 col-lg-6">
-          <SlideRight
-            iterations={Number(animations)}
-            duration={ANIMATION_SPEED}
-          >
+          <SlideRight iterations={Number(animations)} duration={ANIMATION_SPEED}>
             <div className="box">
-              <Ranking
-                style={style}
-                amountToShow={5}
-                rows={cheapWrestlers}
-                columns={COST_COLUMNS}
-                title="Cheaper Wrestlers"
-              />
+              <Ranking style={style} amountToShow={5} rows={cheapWrestlers} columns={COST_COLUMNS} title="Cheaper Wrestlers" />
             </div>
           </SlideRight>
         </div>
@@ -130,16 +77,8 @@ DashboardPage.propTypes = {
 export default connect(state => ({
   cheapWrestlers: sortBy(state.roster, "cost"),
   expensiveWrestlers: sortBy(state.roster, "cost").reverse(),
-  rankedMaleWrestlers: orderBy(
-    state.roster.filter(wrestler => wrestler.male),
-    "points",
-    "desc"
-  ),
-  rankedFemaleWrestlers: orderBy(
-    state.roster.filter(wrestler => !wrestler.male),
-    "points",
-    "desc"
-  ),
+  rankedMaleWrestlers: orderBy(state.roster.filter(wrestler => wrestler.male), "points", "desc"),
+  rankedFemaleWrestlers: orderBy(state.roster.filter(wrestler => !wrestler.male), "points", "desc"),
   roster: state.roster,
   iconColour: state.style.backgroundColor,
   ...state.game,
