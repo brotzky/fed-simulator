37d5dbba0c70c89e83ffd25bd9828a032a5b6199
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-09-28 08:13:11 +0100

    Removed dead tests and improved game model to only contain used properties

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -5,14 +5,7 @@ export default class extends BaseModel {
     return {
       id: false,
       animations: true,
-      canPlan: true,
-      cash: 0,
-      currency: "$",
-      currentDate: new Date().getDate(),
-      currentMonth: new Date().getMonth(),
-      currentYear: new Date().getFullYear(),
       name: "",
-      size: "",
       started: false,
     }
   }
