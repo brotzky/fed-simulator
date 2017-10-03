fc4fff1bfd7eefcd8f067df7298a5482bf2f1b4d
Aaron Lote <aaron.lote@gamesys.co.uk>
2017-10-03 13:30:04 +0100

    Output root as functional component

--- src/root.js
+++ src/root.js
@@ -8,18 +8,16 @@ import configureStore from "./store/configure-store"
 
 const store = configureStore()
 
-export default class Root extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <ContextHolder context={context}>
-          <Router
-            key={new Date()}
-            history={hashHistory}
-            routes={routes()}
-          />
-        </ContextHolder>
-      </Provider>
-    )
-  }
+export default function Root(props) {
+  return (
+    <Provider store={store}>
+      <ContextHolder context={context}>
+        <Router
+          key={new Date()}
+          history={hashHistory}
+          routes={routes()}
+        />
+      </ContextHolder>
+    </Provider>
+  )
 }
