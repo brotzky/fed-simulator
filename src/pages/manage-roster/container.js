f79c396f4195f540775fb55868680202c7e842c7
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-10-28 18:50:31 +0100

    Clear id after toggle creation

--- src/pages/manage-roster/container.js
+++ src/pages/manage-roster/container.js
@@ -18,6 +18,7 @@ export default compose(
     {
       onToggleCreating: ({ creating, }) => () => ({
         creating: !creating,
+        id: false,
       }),
       onSetId: ({ id, }) => () => ({
         id: id,
